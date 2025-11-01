c3498ef8119ac81f8738bcadb522dcff0161986c
David Melo <melodavid84@icloud.com>
2025-10-31 21:02:44 -0300

    bayanasystem

--- front/crypto-monitor-frontend/src/utils/constants.js
+++ front/crypto-monitor-frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || 'https://crypto-monitor-backend.onrender.com/crypto-monitor/api';
+  process.env.REACT_APP_API_URL || 'https://crypto-monitor-api-tkla.onrender.com/crypto-monitor/api';
